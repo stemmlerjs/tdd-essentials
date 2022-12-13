5320614dcb644f8c12cb5be76714a7f28e773853
Khalil Stemmler <metroidman12@gmail.com>
2022-12-12 19:34:51 -0500

    returns "minValue", "maxValue", "numElements", "averageValue"

--- challenges/Module A - Classic (stateless)/2 - Act-Arrange-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ challenges/Module A - Classic (stateless)/2 - Act-Arrange-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -1,29 +1,15 @@
-// import { PalindromeChecker } from './index'
 
-// describe('palindrome checker', () => {
-//   let palindromeChecker: PalindromeChecker;
+import { calculateStats } from './index'
 
-//   beforeEach(() => {
-//     palindromeChecker = new PalindromeChecker();
-//   })
+describe('stats calculator', () => {
 
-//   it('should be able to tell that "mom" is a palindrome', () => {
-//     expect(palindromeChecker.isAPalindrome('mom')).toBeTruthy();
-//   });
+  it('returns "minValue", "maxValue", "numElements", "averageValue"', () => {
+    let response = calculateStats([2, 4, 21, -8, 53, 40]);
 
-//   it('should be able to tell that "bill" isnt a palindrome', () => {
-//     expect(palindromeChecker.isAPalindrome('bill')).toBeFalsy();
-//   });
-
-//   it('should still detect a palindrome even if the casing is off', () => {
-//     expect(palindromeChecker.isAPalindrome("Mom")).toBeTruthy();
-//   });
-
-//   it('should be able to tell that "Was It A Rat I Saw" is a palindrome', () => {
-//     expect(palindromeChecker.isAPalindrome("Was It A Rat I Saw")).toBeTruthy();
-//   });
-
-//   it('should be able to tell that "Never Odd or Even" is palindrome', () => {
-//     expect(palindromeChecker.isAPalindrome("Never Odd or Even")).toBeTruthy();
-//   })
-// })
+    expect(response['minValue']).toBeDefined();
+    expect(response['maxValue']).toBeDefined();
+    expect(response['numElements']).toBeDefined();
+    expect(response['averageValue']).toBeDefined();
+  })
+  
+})
---
+++ challenges/Module A - Classic (stateless)/2 - Act-Arrange-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -0,0 +1,9 @@
+
+export function calculateStats (numArray: number[]) {
+  return {
+    minValue: -1,
+    maxValue: -1,
+    numElements: -1,
+    averageValue: -1
+  }
+}
