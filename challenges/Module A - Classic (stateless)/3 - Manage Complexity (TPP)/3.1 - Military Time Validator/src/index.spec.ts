5eb90d3a0e5d9bd0de5bafc6fc00397a262742c9
Khalil Stemmler <metroidman12@gmail.com>
2022-12-13 13:08:32 -0500

    Prepare for final challenge in Module A3

--- challenges/Module A - Classic (stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
+++ challenges/Module A - Classic (stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
@@ -3,7 +3,33 @@ import { MilitaryTimeValidator } from './index'
 
 describe('military time validator', () => {
 
-  it('throw an error when an empty string is provided', () => {
-    expect(() => MilitaryTimeValidator.validate("")).toThrow();
+  it('knows that an empty string is not a valid time', () => {
+    expect(MilitaryTimeValidator.validate("")).toBeFalsy();
+  });
+
+  it('knows that "010011:00" is not a valid military time', () => {
+    expect(MilitaryTimeValidator.validate("010011:00")).toBeFalsy();
   })
+
+  it('knows that "0100- 1:00" is not a valid military time', () => {
+    expect(MilitaryTimeValidator.validate("0100- 1:00")).toBeFalsy();
+  })
+
+  it('knows that "01:00 - 0100" is not a valid military time', () => {
+    expect(MilitaryTimeValidator.validate("01:00 - 0100")).toBeFalsy();
+  })
+
+  it('knows that "0100 - 11:00" is not a valid military time', () => {
+    expect(MilitaryTimeValidator.validate("0100 - 11:00")).toBeFalsy();
+  })
+
+  it('knows that "01:00 - 11:00" is a valid military time', () => {
+    expect(MilitaryTimeValidator.validate("01:00 - 11:00")).toBeTruthy();
+  })
+
+  it('knows that "20:00 - 11:00" is not valid military time', () => {
+    expect(MilitaryTimeValidator.validate("20:00 - 11:00")).toBeFalsy();
+  })
+
+  // ... Continue
 })
