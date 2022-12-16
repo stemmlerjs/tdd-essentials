f3fcbcb29e5165bb66b345a017711d76d3eeb473
Khalil Stemmler <metroidman12@gmail.com>
2022-12-16 15:50:06 -0500

    knows that the game is not a draw when there are still slots left

--- challenges/Module B - Inside-Out (classic-stateful)/4 - State Machines/4.1 - Tic Tac Toe/src/index.spec.ts
+++ challenges/Module B - Inside-Out (classic-stateful)/4 - State Machines/4.1 - Tic Tac Toe/src/index.spec.ts
@@ -3,29 +3,23 @@ import { TicTacToe } from './index'
 
 describe('tic tac toe', () => {
 
-  it("should be player 1's turn when the game starts", () => {
-    let game: TicTacToe;
+  let game: TicTacToe;
 
+  beforeEach(() => {
     game = new TicTacToe();
+  })
 
+  it("should be player 1's turn when the game starts", () => {
     expect(game.getTurn()).toEqual('playerOne')
   });
 
   it("should be player 2's turn after player 1 moves", () => {
-    let game: TicTacToe;
-
-    game = new TicTacToe();
-
     game.move('topLeft');
 
     expect(game.getTurn()).toEqual('playerTwo')
   });
 
   it("should alternate between player 1 and 2", () => {
-    let game: TicTacToe;
-
-    game = new TicTacToe();
-
     game.move('topLeft');
     expect(game.getTurn()).toEqual('playerTwo');
 
@@ -34,6 +28,25 @@ describe('tic tac toe', () => {
 
     game.move('topMiddle');
     expect(game.getTurn()).toEqual('playerTwo');
+  });
+
+  it("knows that the game is a draw when all of the slots have been filled", () => {
+    game.move('topLeft');
+    game.move('topMiddle');
+    game.move('topRight');
+    game.move('middleLeft');
+    game.move('middle');
+    game.move('middleRight');
+    game.move('bottomLeft');
+    game.move('bottomMiddle');
+    game.move('bottomRight');
+
+    expect(game.isOver()).toBeTruthy();
+  });
+
+  it("knows that the game is not a draw when there are still slots left", () => {
+    game.move('topLeft');
+    expect(game.isOver()).toBeFalsy();
   })
 
 })
--- challenges/Module B - Inside-Out (classic-stateful)/4 - State Machines/4.1 - Tic Tac Toe/src/index.ts
+++ challenges/Module B - Inside-Out (classic-stateful)/4 - State Machines/4.1 - Tic Tac Toe/src/index.ts
@@ -1,22 +1,63 @@
 
 type Player = 'playerOne' | 'playerTwo'
 
-type Position = 'topLeft' | 'topRight' | 'topMiddle'
+type Position = 'topLeft' | 'topRight' | 'topMiddle' |
+  'middleLeft' | 'middle' | 'middleRight' | 'bottomLeft' |
+  'bottomMiddle' | 'bottomRight';
+
+type Slot = 'x' | 'o' | ''
 
 export class TicTacToe {
 
   private turn: Player;
+  private grid: Slot[][];
+  private slotsLeft: number = 9;
 
   constructor () {
     this.turn = 'playerOne';
+    this.grid = [...Array<Slot>(3)].map(e => Array<Slot>(3).fill(''));
   }
 
   getTurn (): Player {
     return this.turn;
   }
 
+  private getSlotCoordinateFromPosition (position: Position): [number, number] {
+    switch (position) {
+      case 'topLeft':
+        return [0, 0];
+      case 'topRight':
+        return [0, 1];
+      case 'topMiddle':
+        return [0, 2];
+      case 'middleLeft':
+        return [1, 0];
+      case 'middle':
+        return [1, 1];
+      case 'middleRight':
+        return [1, 2];
+      case 'bottomLeft':
+        return [2, 0];
+      case 'bottomMiddle':
+        return [2, 1];
+      case 'bottomRight':
+        return [2, 2];
+      default:
+        throw new Error('Invalid position')  
+    }
+  }
+
+
   move (position: Position): void {
     this.turn = this.turn === 'playerOne' ? 'playerTwo' : 'playerOne';
+
+    let [x, y] = this.getSlotCoordinateFromPosition(position)
+    this.grid[x][y] = this.turn === 'playerOne' ? 'x' : 'o';
+    this.slotsLeft--;
+  }
+
+  isOver () {
+    return this.slotsLeft === 0;
   }
 
 }
