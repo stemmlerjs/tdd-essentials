5acd68b6058d2810c350b570faea920580f28f38
Khalil Stemmler <metroidman12@gmail.com>
2022-12-20 04:38:53 -0500

    knows that "NOT TRUE" equates to false

--- solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.spec.ts
+++ solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.spec.ts
@@ -1,17 +1,37 @@
 import { BooleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
-  it('knows that "true" equates to true', () => {
-    expect(BooleanCalculator.calculate("true")).toBeTruthy();
-  });
 
-  it('knows that "false" equates to false', () => {
-    expect(BooleanCalculator.calculate("false")).toBeFalsy();
-  });
+  describe('single values', () => {
+    it('knows that "true" equates to true', () => {
+      expect(BooleanCalculator.calculate("true")).toBeTruthy();
+    });
+  
+    it('knows that "false" equates to false', () => {
+      expect(BooleanCalculator.calculate("false")).toBeFalsy();
+    });
+  
+    it("treats 'true', 'TRUE', 'trUE', and 'True' %s as a valid 'true'", () => {
+      ["true", "TRUE", "trUE", "True"].forEach((t) =>
+        expect(BooleanCalculator.calculate(t)).toBeTruthy()
+      );
+    });
+  
+    it("treats 'false', 'FALSE', 'fAlSe', and 'False' %s as a valid 'false'", () => {
+      ['false', 'FALSE', 'fAlSe', "False"].forEach((f) =>
+        expect(BooleanCalculator.calculate(f)).toBeFalsy()
+      );
+    });
+  })
 
-  it("treats 'true', 'TRUE', 'trUE', and 'True' %s as a valid 'true'", () => {
-    ["true", "TRUE", "trUE", "True"].forEach((t) =>
-      expect(BooleanCalculator.calculate(t)).toBeTruthy()
-    );
-  });
+  describe('not operator', () => {
+    it('knows that "NOT TRUE" equates to false', () => {
+      expect(BooleanCalculator.calculate("NOT TRUE")).toBeFalsy();
+    })
+
+    it('knows that "NOT FALSE" equates to true', () => {
+      expect(BooleanCalculator.calculate("NOT FALSE")).toBeTruthy();
+    })
+  })
+  
 });
--- solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.ts
+++ solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.ts
@@ -1,9 +1,14 @@
 
+const isNotExpression = (parts: string[]) => parts[0].toLowerCase() === 'not';
+const isTrue = (expression: string) => expression.toLowerCase() === 'true'
+
 export class BooleanCalculator {
 
   public static calculate (expression: string): boolean {
-    let isTrue = expression.toLowerCase() === 'true';
-    return isTrue;
+    let parts = expression.split(" ");
+    if (parts.length === 1) return isTrue(expression);
+    if (isNotExpression(parts)) return !isTrue(parts[1])
+    return true
   }
 
 }
