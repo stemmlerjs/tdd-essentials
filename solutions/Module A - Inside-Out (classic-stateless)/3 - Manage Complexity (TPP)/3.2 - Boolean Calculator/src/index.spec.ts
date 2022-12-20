adc96f0d4149196f8d79c0e4751fa208398bd734
Khalil Stemmler <metroidman12@gmail.com>
2022-12-20 04:04:53 -0500

    knows that "true" equates to true

--- solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.spec.ts
+++ solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.spec.ts
@@ -1,35 +1,22 @@
 
-import { MilitaryTimeValidator } from './index'
+import { BooleanCalculator } from './index'
 
-describe('military time validator', () => {
+describe('boolean calculator', () => {
 
-  it('knows that an empty string is not a valid time', () => {
-    expect(MilitaryTimeValidator.validate("")).toBeFalsy();
-  });
-
-  it('knows that "010011:00" is not a valid military time', () => {
-    expect(MilitaryTimeValidator.validate("010011:00")).toBeFalsy();
+  it('knows that "true" equates to true', () => {
+    expect(BooleanCalculator.calculate('true')).toBeTruthy();
   })
 
-  it('knows that "0100- 1:00" is not a valid military time', () => {
-    expect(MilitaryTimeValidator.validate("0100- 1:00")).toBeFalsy();
-  })
+  // it('knows that "false" equates to false', () => {
 
-  it('knows that "01:00 - 0100" is not a valid military time', () => {
-    expect(MilitaryTimeValidator.validate("01:00 - 0100")).toBeFalsy();
-  })
+  // })
 
-  it('knows that "0100 - 11:00" is not a valid military time', () => {
-    expect(MilitaryTimeValidator.validate("0100 - 11:00")).toBeFalsy();
-  })
+  // it("can handle upper, mixed, and lowercase versions of 'true'", () => {
 
-  it('knows that "01:00 - 11:00" is a valid military time', () => {
-    expect(MilitaryTimeValidator.validate("01:00 - 11:00")).toBeTruthy();
-  })
+  // })
 
-  it('knows that "20:00 - 11:00" is not valid military time', () => {
-    expect(MilitaryTimeValidator.validate("20:00 - 11:00")).toBeFalsy();
-  })
+  // it("can handle upper, mixed, and lowercase versions of 'false'", () => {
 
-  // ... Continue
+  // });
+
 })
--- solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.ts
+++ solutions/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.2 - Boolean Calculator/src/index.ts
@@ -1,17 +1,8 @@
 
-const leftSide = (text: string) => text.split(' - ')[0];
-const rightSide = (text: string) => text.split(' - ')[1];
-const hour = (text: string) => text.split(":")[0];
+export class BooleanCalculator {
 
-export class MilitaryTimeValidator {
-  public static validate (text: string): boolean {
-    if (text === "") return false;
-    if (text.indexOf(" - ") === -1) return false;
-    if (leftSide(text).indexOf(':') === -1) return false; 
-    if (rightSide(text).indexOf(':') === -1) return false; 
-    if (hour(leftSide(text)) > hour(rightSide(text))) return false;
-  
+  public static calculate (expression: string): boolean {
     return true;
   }
-}
 
+}
